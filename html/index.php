f753bc4074f0bc164fbbc2f9a6684c7495f5209a
David <david.gwynne@ntlworld.com>
2019-12-04 10:29:48 +0000

    updated ports

--- html/index.php
+++ html/index.php
@@ -8,6 +8,8 @@
 	<a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>:3000">ISM Prototype</a>
 	<a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>:3500">Wavebuoy Prototype</a> 
 	<a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>:3360">Outfit Prototype</a> 
+	<a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>:3250">Bath Live Trains</a>
+	<a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>:3250">Smart Meter</a>
 </html>
 
 <p> Welcome </p>
